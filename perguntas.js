1814b7738b86e63904002438737491346124803c
TatazinhoDoGrau <168441258+TatazinhoDoGrau@users.noreply.github.com>
2024-11-29 13:34:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'matemática',
+    'qual é a raiz quadrada de 81 vezes 7 ?',
+    '63'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o pais menos populoso do mundo?',
+    'Vaticano'
+)
+
+criaCartao(
+    'Química',
+    'O que é o número atômico de um elemento?',
+    'É o número de prótons no núcleo de um átomo, que define o elemento químico.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a forma interrogativa do verbo "to be" com o pronome "they"?',
+    'Are they?'
+)
